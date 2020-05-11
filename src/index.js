614cb36b7d54e9cd36c1d9ef382ed44cdedc7115
ashutipath <ashutosh.ext@dunzo.in>
2020-05-11 16:00:51 +0530

    removed logger

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistStore, persistReducer } from 'redux-persist';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 
 import pReducer from './redux/reducer';
@@ -11,32 +11,19 @@ import './index.scss';
 
 import './static/scss/index.scss';
 
-const logger = store => next => action => {
-  console.log('<---- @@@@@ --->');
-  console.log('dispatching', action);
-  console.log('prev state', store.getState());
-  let result = next(action);
-  console.log('next state', store.getState());
-};
-
-// export const store = createStore(pReducer)
-
 const store = createStore(
   pReducer,
-  compose(
-    applyMiddleware(logger),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__({
-        serialize: {
-          options: {
-            undefined: true,
-            function(fn) {
-              return fn.toString();
-            },
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__({
+      serialize: {
+        options: {
+          undefined: true,
+          function(fn) {
+            return fn.toString();
           },
         },
-      }),
-  ),
+      },
+    }),
 );
 
 export const persistor = persistStore(store);
